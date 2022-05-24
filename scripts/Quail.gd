1ccf8775b225bd23a445dee3a60d09362516a529
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-24 17:28:14 -0500

    Added sprite rotation to Quail

--- scripts/Quail.gd
+++ scripts/Quail.gd
@@ -6,16 +6,23 @@ const MAX_SPEED: = 500
 
 var velocity: = Vector2.ZERO
 
-
 func _ready() -> void:
-	pass
+	set_rotation_degrees(0)
 
+func _process(delta: float) -> void:
+	rotation_degrees += 1
+	
+	if (Input.is_action_just_pressed("up")):
+		set_rotation_degrees(-30)
+	
+	if (rotation_degrees > 90):
+		set_rotation_degrees(90)
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY
 	
 	if velocity.y > MAX_SPEED:
-		velocity.y = MAX_SPEED
+		velocity.y = 800
 	if velocity.y < -MAX_SPEED:
 		velocity.y = -MAX_SPEED
 	
